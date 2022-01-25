18eff1aa4575b036099225299c2dbb0ab9f22497
inju2403 <56947879+inju2403@users.noreply.github.com>
2022-01-25 15:24:25 +0900

    chore: 주석 제거

--- OpenMarket/OpenMarket/Models/Repository/OpenMarketRepository.swift
+++ OpenMarket/OpenMarket/Models/Repository/OpenMarketRepository.swift
@@ -6,17 +6,10 @@
 //
 
 protocol OpenMarketRepository {
-    // GET
     func productList(pageNumber: Int, itemPerPage: Int , completion: @escaping (Result<ProductList?, NetworkError>) -> Void)
     func product(productId: Int, completion: @escaping (Result<Product?, NetworkError>) -> Void)
     func productSecret()
-
-    // POST
     func addProduct()
-
-    // PATCH
     func updateProduct()
-
-    // DELETE
     func deleteProduct()
 }
