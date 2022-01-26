ac9422f1739196490084dc70acf7b18de551a107
inju2403 <56947879+inju2403@users.noreply.github.com>
2022-01-26 17:56:20 +0900

    refactor: ProductList 프로퍼티 이름 변경 product -> products

--- OpenMarket/OpenMarket/Models/Product/ProductList.swift
+++ OpenMarket/OpenMarket/Models/Product/ProductList.swift
@@ -14,7 +14,7 @@ struct ProductList: Decodable {
     let lastPage: Int
     let hasNext: Bool
     let hasPrev: Bool
-    let product: [Product]?
+    let products: [Product]?
 
     enum CodingKeys: String, CodingKey {
         case pageNumber = "page_no"
@@ -23,7 +23,7 @@ struct ProductList: Decodable {
         case lastPage = "last_page"
         case hasNext = "has_next"
         case hasPrev = "has_prev"
-        case product = "pages"
+        case products = "pages"
         case offset, limit
     }
 }
