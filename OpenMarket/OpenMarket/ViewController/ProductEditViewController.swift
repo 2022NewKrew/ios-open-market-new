cccc2efda027665993820f28194cd66959ce9a09
Kim Hyun Shik <samerj9712@gmail.com>
2022-02-07 15:07:00 +0900

    feat: step3 상품등록/수정 화면 인터페이스 구성
    - 스토리 보드에서 구현

---
+++ OpenMarket/OpenMarket/ViewController/ProductEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductEditViewController.swift
+//  OpenMarket
+//
+//  Created by kakao on 2022/02/07.
+//
+
+import UIKit
+
+class ProductEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
