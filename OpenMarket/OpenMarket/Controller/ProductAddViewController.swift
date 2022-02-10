baff235a2842ca7a1caddd4b5af5bfbc4ed5e5bf
claire-pp <claire.pp@kakaocorp.com>
2022-02-10 16:18:15 +0900

    refactor: KeyboardSizeControllable 프로토콜 추가
    키보드 사이즈에 따라 컨텐츠 크기가 바뀌어야 하는 경우,
    KeyboardSizeControllable 프로토콜을 이용할 수 있도록 변경

--- OpenMarket/OpenMarket/Controller/ProductAddViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductAddViewController.swift
@@ -1,16 +1,16 @@
 import UIKit
 
-class ProductAddViewController: UIViewController {
-    let addView = ProductAddView()
+class ProductAddViewController: UIViewController, KeyboardSizeControllable {
+    let innerView: ProductEditableView = ProductAddView()
     var onkeyboardConstraints: [NSLayoutConstraint] = []
-    lazy var bottomConstraint = addView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-    
+    lazy var bottomConstraint = innerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(addView)
-        addView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        addView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        addView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        view.addSubview(innerView)
+        innerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        innerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        innerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
         bottomConstraint.isActive = true
         view.backgroundColor = .systemBackground
         navigationController?.title = "상품등록"
@@ -28,7 +28,7 @@ class ProductAddViewController: UIViewController {
     
     @objc
     func addProduct() {
-        addView.saveProduct()
+        (innerView as? ProductAddView)?.saveProduct()
         navigationController?.popViewController(animated: true)
     }
     
@@ -36,40 +36,12 @@ class ProductAddViewController: UIViewController {
     func cancelAdding() {
         navigationController?.popViewController(animated: true)
     }
-    
-    func addKeyboardNotifications(){
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification , object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    func removeKeyboardNotifications(){
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification , object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    
-    @objc
-    func keyboardWillShow(_ notification: NSNotification){
-        if onkeyboardConstraints.count == 0, let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-            let keyboardRectangle = keyboardFrame.cgRectValue
-            let keyboardHeight = keyboardRectangle.height
-            onkeyboardConstraints = [addView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -keyboardHeight)]
-        }
-        NSLayoutConstraint.deactivate([bottomConstraint])
-        NSLayoutConstraint.activate(onkeyboardConstraints)
-    }
-    
-    @objc
-    func keyboardWillHide(_ notification: NSNotification){
-        NSLayoutConstraint.deactivate(onkeyboardConstraints)
-        NSLayoutConstraint.activate([bottomConstraint])
-    }
 }
 
 extension ProductAddViewController: UIImagePickerControllerDelegate {
     @objc
     func presentAlbum(){
-        if addView.imageData.count >= 5 {
+        if innerView.imageData.count >= 5 {
             let okay = UIAlertAction(title: "확인", style: .default, handler: nil)
             let alertController = UIAlertController(title: "오류", message: "이미지는 5개까지만 가능해요", preferredStyle: .alert)
             alertController.addAction(okay)
@@ -85,7 +57,7 @@ extension ProductAddViewController: UIImagePickerControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.editedImage] as? UIImage {
-            addView.addImage(image: image)
+            (innerView as? ProductAddView)?.addImage(image: image)
         }
         dismiss(animated: true, completion: nil)
     }
--- OpenMarket/OpenMarket/Controller/ProductEditViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductEditViewController.swift
@@ -1,14 +1,14 @@
 import UIKit
 
-class ProductEditViewController: UIViewController {
-    let editView: ProductEditView
+class ProductEditViewController: UIViewController, KeyboardSizeControllable {
+    let innerView: ProductEditableView
     var onkeyboardConstraints: [NSLayoutConstraint] = []
-    lazy var bottomConstraint = editView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+    lazy var bottomConstraint = innerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
     
     init(data: Product) {
-        editView = ProductEditView()
+        innerView = ProductEditView()
         super.init(nibName: nil, bundle: nil)
-        editView.setupData(data: data)
+        (innerView as? ProductEditView)?.setupData(data: data)
     }
     
     required init?(coder: NSCoder) {
@@ -17,10 +17,10 @@ class ProductEditViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(editView)
-        editView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        editView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        editView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        view.addSubview(innerView)
+        innerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        innerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        innerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
         bottomConstraint.isActive = true
         view.backgroundColor = .systemBackground
         navigationController?.title = "상품수정"
@@ -45,33 +45,5 @@ class ProductEditViewController: UIViewController {
     @objc
     func cancelAdding() {
         navigationController?.popViewController(animated: true)
-    }
-    
-    func addKeyboardNotifications(){
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification , object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    func removeKeyboardNotifications(){
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification , object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    
-    @objc
-    func keyboardWillShow(_ notification: NSNotification){
-        if onkeyboardConstraints.count == 0, let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-            let keyboardRectangle = keyboardFrame.cgRectValue
-            let keyboardHeight = keyboardRectangle.height
-            onkeyboardConstraints = [editView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -keyboardHeight)]
-        }
-        NSLayoutConstraint.deactivate([bottomConstraint])
-        NSLayoutConstraint.activate(onkeyboardConstraints)
-    }
-    
-    @objc
-    func keyboardWillHide(_ notification: NSNotification){
-        NSLayoutConstraint.deactivate(onkeyboardConstraints)
-        NSLayoutConstraint.activate([bottomConstraint])
     }
 }
